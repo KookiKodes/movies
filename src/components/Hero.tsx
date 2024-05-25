38dbe52c760ebd06813e7fba606c16f0c5488264
Kooki Kodes <kookikodes@gmail.com>
2024-05-25 14:11:37 -0700

    initial hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,5 @@
+import type { FeaturedMedia } from "~/services/app";
+import { formatRuntime } from "~/utils/format";
+import styles from "./Hero.module.css";
+
+export default function Hero(props: FeaturedMedia) {}
