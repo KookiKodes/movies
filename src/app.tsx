f9dd19fd793801dbf0e9f4176db3611fcdf3f640
Kooki Kodes <kookikodes@gmail.com>
2024-05-24 20:46:06 -0700

    navbar function and styling

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,16 @@ import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import GlobalLoader from "./components/GlobalLoader";
+import Nav from "./components/Nav";
+
 import "./styles/base.css";
 import "./styles/forms.css";
 import "./styles/layout.css";
 import "./styles/normalize.css";
 import "./styles/transitions.css";
 import "./styles/typography.css";
-import GlobalLoader from "./components/GlobalLoader";
+import "./styles/helpers.css";
 
 export default function App() {
   return (
@@ -19,6 +22,7 @@ export default function App() {
           <Meta charset="utf-8" />
           <Meta name="viewport" content="width=device-width,initial-scale=1" />
           <GlobalLoader />
+          <Nav />
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
